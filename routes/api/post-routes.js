aec5c8a2a2845789ca6ae6e7a9ea32aaf4dbb85f
rvanooyen <robert.vanooyen@gmail.com>
2021-10-27 16:57:52 -0500

    feature comments complete

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
@@ -13,8 +14,23 @@ router.get('/', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [['created_at', 'DESC']],
         include: [
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes:
+                [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
               model: User,
               attributes: ['username']
@@ -41,12 +57,28 @@ router.get('/:id', (req, res) => {
           'created_at',
           [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
+        include: [
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes:
+                [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
+        ]
     })
       .then(dbPostData => {
         if (!dbPostData) {
