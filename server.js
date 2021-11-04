bfe15ca9d53d473c59eaeb910542f08a78089762
rvanooyen <robert.vanooyen@gmail.com>
2021-11-04 01:52:00 -0500

    feature/clean-codebase

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
